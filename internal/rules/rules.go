fce205027adb383f82a24f0a03197105e1f8b9ce
vahid lotfi <v.lotfi71@gmail.com>
2025-09-21 22:27:45 +0330

    create validation fidle and auth_validation.go for register and login

--- internal/rules/rules.go
+++ internal/rules/rules.go
@@ -3,11 +3,11 @@ package rules
 type Rule func(value string, fieldName string) (passed bool, message string, err error)
 
 type FieldRules struct {
-	FieldName string `json:"field"`
-	Rules     []Rule `json:"rules"`
+	Field string `json:"field"`
+	Rules []Rule `json:"rules"`
 }
 
 type ValidationError struct {
-	Field   string `json:"field"`
+	Field   string `json:"field_name"`
 	Message string `json:"message"`
 }
