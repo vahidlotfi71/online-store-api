07f56097c1a0b9b5b5c647501bf029f028575bbe
vahid lotfi <v.lotfi71@gmail.com>
2025-09-30 21:39:18 +0330

    Create respons.go to manage response

--- internal/utils/response.go
+++ internal/utils/response.go
@@ -1,1 +1,13 @@
 package utils
+
+import "github.com/gofiber/fiber/v2"
+
+func SuccessResponse(c *fiber.Ctx, data interface{}) error {
+	//c *fiber.Ctx → کانتکست درخواست است، یعنی همه اطلاعات مربوط به درخواست و پاسخ تو این شی هست.
+	//data interface{} → داده‌ای که می‌خوایم برگردونیم (هر نوعی می‌تونه باشه).
+	return c.JSON(fiber.Map{"success": true, "data": data})
+}
+
+func ErrorResponse(c *fiber.Ctx, status int, msg string) error {
+	return c.Status(status).JSON(fiber.Map{"success": false, "message": msg})
+}
